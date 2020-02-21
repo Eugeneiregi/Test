82fdc01fae1ee53501fbb2df8b969461393b39f5
Eugeneiregi <eugeneregi2019@gmail.com>
2020-02-21 09:38:30 +0300

    New function

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function take(number, length, retake) {
-    number = number + "";
-    if (number.length < length) {
-        for (var i = number.length; i < length; i += 1) {
-            number = retake + number;
-        }
-    }
+// function take(number, length, retake) {
+//     number = number + "";
+//     if (number.length < length) {
+//         for (var i = number.length; i < length; i += 1) {
+//             number = retake + number;
+//         }
+//     }
 
-    return number;
-}
+//     return number;
+// }
 
 
+function myFunction(){
+    var take = prompt("Put a number");
+    var takeIncreamented = new String();
+    if(isNaN("take")||take<=0){
+    return(take+1);}
+  }
+  myFunction();
